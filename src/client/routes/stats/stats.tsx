5289725fdb64aeab61af5892ca09752fa2028f3e
Per Hansen <me@perhp.com>
2025-07-16 00:54:53 +0200

    feat: show stats image

--- src/client/routes/stats/stats.tsx
+++ src/client/routes/stats/stats.tsx
@@ -1,6 +1,7 @@
 import { Stats } from "@/models/stats.model";
 import { useQuery } from "@tanstack/react-query";
 import { LoaderPinwheel } from "lucide-react";
+import StatsImage from "../../../public/stats.png";
 
 function useStats() {
   return useQuery({
@@ -23,7 +24,7 @@ export default function Stats() {
 
   if (statsIsLoading) {
     return (
-      <div className="flex flex-col h-screen bg-slate-800">
+      <div className="flex flex-col h-screen bg-black">
         <div className="grid items-center text-2xl font-bold text-gray-100 place-content-center grow">
           <LoaderPinwheel className="animate-spin size-14" />
         </div>
@@ -35,11 +36,14 @@ export default function Stats() {
     return <div>Error</div>;
   }
 
-  console.log(stats);
-
   return (
-    <div>
-      <img src="./public/stats.png" />
+    <div className="grid w-full h-screen">
+      <div className="aspect-[204/275] max-h-screen col-start-1 row-start-1 mx-auto bg-red-500/25 z-10"></div>
+      <img
+        src={StatsImage}
+        alt=""
+        className="object-contain w-full h-full max-h-screen col-start-1 row-start-1"
+      />
     </div>
   );
 }
