2032c8f7126cb20d258be007532b22bd5958373e
Per Hansen <me@perhp.com>
2025-07-16 12:39:29 +0200

    fix: use env port

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -4,6 +4,7 @@ import buildRoutes from "./routes";
 const server = serve({
   routes: buildRoutes(),
   development: process.env.NODE_ENV !== "production",
+  port: process.env.PORT ?? 3000,
 });
 
 console.log(`🚀  Listening on ${server.url}`);
